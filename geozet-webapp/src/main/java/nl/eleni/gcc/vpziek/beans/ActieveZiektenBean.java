b89649e67ee663c5c5a470ed855ad756c8ba25e8
Mark <mc.prins@gmail.com>
2013-04-26 18:00:16 +0200

    fix geocoder en upgrade geotools + plugins

--- geozet-webapp/src/main/java/nl/eleni/gcc/vpziek/beans/ActieveZiektenBean.java
+++ geozet-webapp/src/main/java/nl/eleni/gcc/vpziek/beans/ActieveZiektenBean.java
@@ -33,184 +33,184 @@ import org.opengis.filter.expression.Function;
  * gaten te houden.
  */
 public class ActieveZiektenBean {
-    /** log4j logger. */
-    private static final Logger LOGGER = Logger
-            .getLogger(ActieveZiektenBean.class);
+	/** log4j logger. */
+	private static final Logger LOGGER = Logger
+			.getLogger(ActieveZiektenBean.class);
 
-    /** virtuele feature collection. */
-    private SimpleFeatureCollection featColl = null;
+	/** virtuele feature collection. */
+	private MemoryFeatureCollection featColl = null;
 
-    /**
-     * elementen van de virtuele feature collection.
-     * 
-     * @see #featColl
-     */
-    private List<String> elements = null;
+	/**
+	 * elementen van de virtuele feature collection.
+	 * 
+	 * @see #featColl
+	 */
+	private List<String> elements = null;
 
-    /**
-     * status vlaggetje voor inititialisatie status.
-     * 
-     * @see #init(SimpleFeatureSource, SimpleFeatureSource, String, String)
-     */
-    private boolean init = false;
+	/**
+	 * status vlaggetje voor inititialisatie status.
+	 * 
+	 * @see #init(SimpleFeatureSource, SimpleFeatureSource, String, String)
+	 */
+	private boolean init = false;
 
-    /**
-     * default constructor voor actieve ziekten bean.
-     */
-    public ActieveZiektenBean() {
-    }
+	/**
+	 * default constructor voor actieve ziekten bean.
+	 */
+	public ActieveZiektenBean() {
+	}
 
-    /**
-     * Inits the bean.
-     * 
-     * @param source
-     *            the source
-     * @param source2
-     *            the source2
-     * @param typeName
-     *            the type name
-     * @param typeName2
-     *            the type name2
-     */
-    @SuppressWarnings("unchecked")
-    public void init(SimpleFeatureSource source, SimpleFeatureSource source2,
-            String typeName, String typeName2) {
-        final String filterString = FEATURE_ATTR_NAAM_EINDDATUM + " IS NULL";
-        try {
-            LOGGER.debug("CQL voor filter is: " + filterString);
-            // query maken
-            final Query query = new Query();
-            query.setTypeName(typeName);
-            query.setFilter(CQL.toFilter(filterString));
-            // query.setCoordinateSystem(CRS.decode("EPSG:28992"));
-            query.setPropertyNames(new String[] { FILTER_BESMETTING_NAAM.code });
-            query.setHandle("ActieveZiektenBean#init");
+	/**
+	 * Inits the bean.
+	 * 
+	 * @param source
+	 *            the source
+	 * @param source2
+	 *            the source2
+	 * @param typeName
+	 *            the type name
+	 * @param typeName2
+	 *            the type name2
+	 */
+	@SuppressWarnings("unchecked")
+	public void init(SimpleFeatureSource source, SimpleFeatureSource source2,
+			String typeName, String typeName2) {
+		final String filterString = FEATURE_ATTR_NAAM_EINDDATUM + " IS NULL";
+		try {
+			LOGGER.debug("CQL voor filter is: " + filterString);
+			// query maken
+			final Query query = new Query();
+			query.setTypeName(typeName);
+			query.setFilter(CQL.toFilter(filterString));
+			// query.setCoordinateSystem(CRS.decode("EPSG:28992"));
+			query.setPropertyNames(new String[] { FILTER_BESMETTING_NAAM.code });
+			query.setHandle("ActieveZiektenBean#init");
 
-            // data ophalen
-            final SimpleFeatureCollection features = source.getFeatures(query);
-            LOGGER.debug("Er zijn " + features.size() + " features opgehaald.");
+			// data ophalen
+			final SimpleFeatureCollection features = source.getFeatures(query);
+			LOGGER.debug("Er zijn " + features.size() + " features opgehaald.");
 
-            query.setTypeName(typeName2);
-            final SimpleFeatureCollection features2 = source2
-                    .getFeatures(query);
-            LOGGER.debug("Er zijn " + features2.size() + " features opgehaald.");
+			query.setTypeName(typeName2);
+			final SimpleFeatureCollection features2 = source2
+					.getFeatures(query);
+			LOGGER.debug("Er zijn " + features2.size() + " features opgehaald.");
 
-            final FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
-            final Function distinct = ff.function("Collection_Unique",
-                    ff.property(FILTER_BESMETTING_NAAM.code));
+			final FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
+			final Function distinct = ff.function("Collection_Unique",
+					ff.property(FILTER_BESMETTING_NAAM.code));
 
-            final TreeSet<String> values = new TreeSet<String>(
-                    (Set<String>) distinct.evaluate(features));
-            LOGGER.debug(values);
-            values.addAll(((Set<String>) distinct.evaluate(features2)));
-            LOGGER.debug(values);
+			final TreeSet<String> values = new TreeSet<String>(
+					(Set<String>) distinct.evaluate(features));
+			LOGGER.debug(values);
+			values.addAll(((Set<String>) distinct.evaluate(features2)));
+			LOGGER.debug(values);
 
-            final SimpleFeatureType sfType = DataUtilities.createType("ziekte",
-                    "geom:Point:srid=28992," + FILTER_BESMETTING_NAAM.code
-                            + ":String");
-            this.featColl = new MemoryFeatureCollection(sfType);
+			final SimpleFeatureType sfType = DataUtilities.createType("ziekte",
+					"geom:Point:srid=28992," + FILTER_BESMETTING_NAAM.code
+							+ ":String");
+			this.featColl = new MemoryFeatureCollection(sfType);
 
-            int i = 0;
-            for (final String s : values) {
-                final Object[] attr = { null, s };
-                this.featColl.add(new SimpleFeatureImpl(attr, sfType,
-                        new FeatureIdImpl("" + i++), false));
-            }
-            this.init = true;
-        } catch (final SchemaException e) {
-            LOGGER.error(e);
-        } catch (final CQLException e) {
-            LOGGER.error("CQL Fout in de query voor de WFS.", e);
-        } catch (final IOException e) {
-            LOGGER.fatal("I/O Fout bij benaderen van de WFS.", e);
-        } /*
-           * catch (final NoSuchAuthorityCodeException e) {
-           * LOGGER.error("De gevraagde CRS autoriteit is niet gevonden.", e); }
-           * catch (final FactoryException e) { LOGGER.error(
-           * "Gevraagde GeoTools factory voor CRS is niet gevonden.", e); }
-           */
-    }
+			int i = 0;
+			for (final String s : values) {
+				final Object[] attr = { null, s };
+				this.featColl.add(new SimpleFeatureImpl(attr, sfType,
+						new FeatureIdImpl("" + i++), false));
+			}
+			this.init = true;
+		} catch (final SchemaException e) {
+			LOGGER.error(e);
+		} catch (final CQLException e) {
+			LOGGER.error("CQL Fout in de query voor de WFS.", e);
+		} catch (final IOException e) {
+			LOGGER.fatal("I/O Fout bij benaderen van de WFS.", e);
+		} /*
+		 * catch (final NoSuchAuthorityCodeException e) {
+		 * LOGGER.error("De gevraagde CRS autoriteit is niet gevonden.", e); }
+		 * catch (final FactoryException e) { LOGGER.error(
+		 * "Gevraagde GeoTools factory voor CRS is niet gevonden.", e); }
+		 */
+	}
 
-    /**
-     * Return the initialized state of this bean, must be {@code true} to use
-     * the bean.
-     * 
-     * @return {@code true} when initialzed {@code false} otherwise
-     */
-    public boolean getInit() {
-        return this.init;
-    }
+	/**
+	 * Return the initialized state of this bean, must be {@code true} to use
+	 * the bean.
+	 * 
+	 * @return {@code true} when initialzed {@code false} otherwise
+	 */
+	public boolean getInit() {
+		return this.init;
+	}
 
-    /**
-     * Gets the feature collection.
-     * 
-     * @return the featColl
-     */
-    public SimpleFeatureCollection getFeatureCollection() {
-        return this.featColl;
-    }
+	/**
+	 * Gets the feature collection.
+	 * 
+	 * @return the featColl
+	 */
+	public SimpleFeatureCollection getFeatureCollection() {
+		return this.featColl;
+	}
 
-    /**
-     * Geeft een lijst van unieke SimpleFeatures die geen locatie hebben en
-     * alleen de attribuut besmetting. Dit simuleert een DISTINCT query.
-     * 
-     * @return Een lijst van SimpleFeatures
-     * 
-     * @todo cache implementeren, bijv. voor 5 minuten zodat we niet iedere keer
-     *       naar de WFS toe hoeven.
-     */
-    public List<SimpleFeature> getActieveZiekten() {
-        final SimpleFeatureIterator iterator = this.featColl.features();
-        final Vector<SimpleFeature> feats = new Vector<SimpleFeature>();
-        while (iterator.hasNext()) {
-            feats.add(iterator.next());
-        }
-        iterator.close();
-        return feats;
-    }
+	/**
+	 * Geeft een lijst van unieke SimpleFeatures die geen locatie hebben en
+	 * alleen de attribuut besmetting. Dit simuleert een DISTINCT query.
+	 * 
+	 * @return Een lijst van SimpleFeatures
+	 * 
+	 * @todo cache implementeren, bijv. voor 5 minuten zodat we niet iedere keer
+	 *       naar de WFS toe hoeven.
+	 */
+	public List<SimpleFeature> getActieveZiekten() {
+		final SimpleFeatureIterator iterator = this.featColl.features();
+		final Vector<SimpleFeature> feats = new Vector<SimpleFeature>();
+		while (iterator.hasNext()) {
+			feats.add(iterator.next());
+		}
+		iterator.close();
+		return feats;
+	}
 
-    /**
-     * Geeft de elementen in deze verzameling.
-     * 
-     * @return de {@code List<String>} met ziekten
-     */
-    public List<String> getElements() {
-        if (this.elements == null) {
-            final SimpleFeatureIterator iterator = this.featColl.features();
-            this.elements = new Vector<String>();
-            while (iterator.hasNext()) {
-                this.elements.add((String) iterator.next().getAttribute(
-                        FILTER_BESMETTING_NAAM.code));
-            }
-            iterator.close();
-            this.elements = Collections.unmodifiableList(this.elements);
-        }
-        return this.elements;
-    }
+	/**
+	 * Geeft de elementen in deze verzameling.
+	 * 
+	 * @return de {@code List<String>} met ziekten
+	 */
+	public List<String> getElements() {
+		if (this.elements == null) {
+			final SimpleFeatureIterator iterator = this.featColl.features();
+			this.elements = new Vector<String>();
+			while (iterator.hasNext()) {
+				this.elements.add((String) iterator.next().getAttribute(
+						FILTER_BESMETTING_NAAM.code));
+			}
+			iterator.close();
+			this.elements = Collections.unmodifiableList(this.elements);
+		}
+		return this.elements;
+	}
 
-    /**
-     * Gets the as html.
-     * 
-     * @return the as html
-     */
-    public String getAsHTML() {
-        final StringBuilder sb = new StringBuilder();
-        int i = 0;
-        // sb.append("<ul>");
-        for (final SimpleFeature f : this.getActieveZiekten()) {
-            sb.append("<li class=\"ziekte\">");
-            sb.append("<input name=\"filter\" value=\""
-                    + f.getAttribute(FILTER_BESMETTING_NAAM.code)
-                    + "\" type=\"checkbox\" checked=\"checked\" id=\"besmet-"
-                    + i + "\"/>");
-            sb.append("<label for=\"besmet-" + i + "\">"
-                    + f.getAttribute(FILTER_BESMETTING_NAAM.code) + "</label>");
-            sb.append("</li>\n");
-            i++;
-        }
-        // sb.append("</ul>");
+	/**
+	 * Gets the as html.
+	 * 
+	 * @return the as html
+	 */
+	public String getAsHTML() {
+		final StringBuilder sb = new StringBuilder();
+		int i = 0;
+		// sb.append("<ul>");
+		for (final SimpleFeature f : this.getActieveZiekten()) {
+			sb.append("<li class=\"ziekte\">");
+			sb.append("<input name=\"filter\" value=\""
+					+ f.getAttribute(FILTER_BESMETTING_NAAM.code)
+					+ "\" type=\"checkbox\" checked=\"checked\" id=\"besmet-"
+					+ i + "\"/>");
+			sb.append("<label for=\"besmet-" + i + "\">"
+					+ f.getAttribute(FILTER_BESMETTING_NAAM.code) + "</label>");
+			sb.append("</li>\n");
+			i++;
+		}
+		// sb.append("</ul>");
 
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 
 }
--- geozet-webapp/src/test/java/nl/eleni/gcc/vpziek/beans/ActieveZiektenBeanTest.java
+++ geozet-webapp/src/test/java/nl/eleni/gcc/vpziek/beans/ActieveZiektenBeanTest.java
@@ -28,103 +28,102 @@ import org.opengis.feature.simple.SimpleFeatureType;
  * 
  */
 public class ActieveZiektenBeanTest {
-    private ActieveZiektenBean initedBean;
-    private final String[] typeNames = new String[] { "type0", "type1" };
-    private SimpleFeatureSource source0;
-    private SimpleFeatureSource source1;
-    private final int numberOfFeatures = 7;
+	private ActieveZiektenBean initedBean;
+	private final String[] typeNames = new String[] { "type0", "type1" };
+	private SimpleFeatureSource source0;
+	private SimpleFeatureSource source1;
+	private final int numberOfFeatures = 7;
 
-    @Before
-    public void setUp() throws Exception {
-        // set up feature collections + sources
-        final SimpleFeatureType TYPE = DataUtilities.createType("ziekte",
-                "geom:Point:srid=28992," + FILTER_BESMETTING_NAAM.code
-                        + ":String");
-        final SimpleFeatureCollection collection0 = new MemoryFeatureCollection(
-                TYPE);
-        final SimpleFeatureCollection collection1 = new MemoryFeatureCollection(
-                TYPE);
-        for (int i = 0, k = 0; i < this.numberOfFeatures; i++) {
-            collection0.add(new SimpleFeatureImpl(new Object[] { null,
-                    "atr" + k++ }, TYPE, new FeatureIdImpl("" + i), false));
+	@Before
+	public void setUp() throws Exception {
+		// set up feature collections + sources
+		final SimpleFeatureType TYPE = DataUtilities.createType("ziekte",
+				"geom:Point:srid=28992," + FILTER_BESMETTING_NAAM.code
+						+ ":String");
+		final MemoryFeatureCollection collection0 = new MemoryFeatureCollection(
+				TYPE);
+		final MemoryFeatureCollection collection1 = new MemoryFeatureCollection(
+				TYPE);
+		for (int i = 0, k = 0; i < this.numberOfFeatures; i++) {
+			collection0.add(new SimpleFeatureImpl(new Object[] { null,
+					"atr" + k++ }, TYPE, new FeatureIdImpl("" + i), false));
 
-            collection1.add(new SimpleFeatureImpl(new Object[] { null,
-                    "atr" + k++ }, TYPE, new FeatureIdImpl("" + i), false));
-        }
-        this.source0 = new CollectionFeatureSource(collection0);
-        this.source1 = new CollectionFeatureSource(collection1);
+			collection1.add(new SimpleFeatureImpl(new Object[] { null,
+					"atr" + k++ }, TYPE, new FeatureIdImpl("" + i), false));
+		}
+		this.source0 = new CollectionFeatureSource(collection0);
+		this.source1 = new CollectionFeatureSource(collection1);
 
-        this.initedBean = new ActieveZiektenBean();
-        this.initedBean.init(this.source0, this.source1, this.typeNames[0],
-                this.typeNames[1]);
-    }
+		this.initedBean = new ActieveZiektenBean();
+		this.initedBean.init(this.source0, this.source1, this.typeNames[0],
+				this.typeNames[1]);
+	}
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@After
+	public void tearDown() throws Exception {
+	}
 
-    @Test
-    public final void testActieveZiektenBean() {
-        assertNotNull(this.initedBean);
-        assertTrue(this.initedBean.getInit());
-    }
+	@Test
+	public final void testActieveZiektenBean() {
+		assertNotNull(this.initedBean);
+		assertTrue(this.initedBean.getInit());
+	}
 
-    /**
-     * Test methode voor
-     * {@link ActieveZiektenBean#init(SimpleFeatureSource, SimpleFeatureSource, String, String)}
-     * en {@link ActieveZiektenBean#getInit() }
-     */
-    @Test
-    public final void testInit() {
-        assertTrue(this.initedBean.getInit());
+	/**
+	 * Test methode voor
+	 * {@link ActieveZiektenBean#init(SimpleFeatureSource, SimpleFeatureSource, String, String)}
+	 * en {@link ActieveZiektenBean#getInit() }
+	 */
+	@Test
+	public final void testInit() {
+		assertTrue(this.initedBean.getInit());
 
-        final ActieveZiektenBean bean = new ActieveZiektenBean();
-        assertFalse(bean.getInit());
-        bean.init(this.source0, this.source1, this.typeNames[0],
-                this.typeNames[1]);
-        assertTrue(bean.getInit());
+		final ActieveZiektenBean bean = new ActieveZiektenBean();
+		assertFalse(bean.getInit());
+		bean.init(this.source0, this.source1, this.typeNames[0],
+				this.typeNames[1]);
+		assertTrue(bean.getInit());
+	}
 
-    }
+	/**
+	 * Test methode voor {@link ActieveZiektenBean#getFeatureCollection()}
+	 */
+	@Test
+	public final void testGetFeatureCollection() {
+		final SimpleFeatureCollection sfc = this.initedBean
+				.getFeatureCollection();
+		assertNotNull(sfc);
+		assertFalse(sfc.isEmpty());
+	}
 
-    /**
-     * Test methode voor {@link ActieveZiektenBean#getFeatureCollection()}
-     */
-    @Test
-    public final void testGetFeatureCollection() {
-        final SimpleFeatureCollection sfc = this.initedBean
-                .getFeatureCollection();
-        assertNotNull(sfc);
-        assertFalse(sfc.isEmpty());
-    }
+	/**
+	 * Test methode voor {@link ActieveZiektenBean#getAsHTML() }
+	 */
+	@Test
+	public final void testGetActieveZiekten() {
+		final List<SimpleFeature> sf = this.initedBean.getActieveZiekten();
+		assertNotNull(sf);
+		assertEquals(2 * this.numberOfFeatures, sf.size());
+	}
 
-    /**
-     * Test methode voor {@link ActieveZiektenBean#getAsHTML() }
-     */
-    @Test
-    public final void testGetActieveZiekten() {
-        final List<SimpleFeature> sf = this.initedBean.getActieveZiekten();
-        assertNotNull(sf);
-        assertEquals(2 * this.numberOfFeatures, sf.size());
-    }
+	/**
+	 * Test methode voor {@link ActieveZiektenBean#getElements() }
+	 */
+	@Test
+	public final void testGetElements() {
+		assertNotNull(this.initedBean.getElements());
+		assertEquals(2 * this.numberOfFeatures, this.initedBean.getElements()
+				.size());
+	}
 
-    /**
-     * Test methode voor {@link ActieveZiektenBean#getElements() }
-     */
-    @Test
-    public final void testGetElements() {
-        assertNotNull(this.initedBean.getElements());
-        assertEquals(2 * this.numberOfFeatures, this.initedBean.getElements()
-                .size());
-    }
-
-    /**
-     * Test methode voor {@link ActieveZiektenBean#getAsHTML() }
-     */
-    @Test
-    public final void testGetAsHTML() {
-        final String s = this.initedBean.getAsHTML();
-        assertNotNull(s);
-        assertTrue(s.startsWith("<li class=\"ziekte\">"));
-    }
+	/**
+	 * Test methode voor {@link ActieveZiektenBean#getAsHTML() }
+	 */
+	@Test
+	public final void testGetAsHTML() {
+		final String s = this.initedBean.getAsHTML();
+		assertNotNull(s);
+		assertTrue(s.startsWith("<li class=\"ziekte\">"));
+	}
 
 }
